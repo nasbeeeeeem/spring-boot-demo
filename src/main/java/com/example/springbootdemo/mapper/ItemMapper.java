26d3183140294dc850b7cfd6eea5cf34bd57051e
nas <vividnasubi@gmail.com>
2024-04-16 15:42:59 +0900

    feat: Mapperインターフェースに対応するCRUDメソッドの実装

--- src/main/java/com/example/springbootdemo/mapper/ItemMapper.java
+++ src/main/java/com/example/springbootdemo/mapper/ItemMapper.java
@@ -1,6 +1,9 @@
 package com.example.springbootdemo.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.springbootdemo.entity.Item;
@@ -8,4 +11,8 @@ import com.example.springbootdemo.entity.Item;
 @Mapper
 public interface ItemMapper {
   Item findById(int id);
+  List<Item> findAll();
+  int insert(@Param("item") Item item);
+  int update(@Param("item") Item item);
+  boolean delete(int id);
 }
