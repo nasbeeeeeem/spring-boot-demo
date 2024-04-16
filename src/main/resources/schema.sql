72ac801159da5be4bb51a47119f3345c92bb5268
nas <vividnasubi@gmail.com>
2024-04-16 15:43:23 +0900

    chor: sqlの整理

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,7 @@
 drop table if exists item;
 
 create table item (
-  id integer,
+  id integer not null AUTO_INCREMENT,
   item_name varchar(20),
   primary key(id)
 );
-
-insert into item (id, item_name) values (1, '大豆');
-insert into item (id, item_name) values (2, '小豆');
